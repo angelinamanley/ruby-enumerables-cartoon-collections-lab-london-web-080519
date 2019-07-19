a172a9e808fc33eef19adf85cc5083008cc536eb
Angelina Shin Manley <ashin05@gmail.com>
2019-07-19 13:46:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,9 @@ end
 
 def summon_captain_planet(planeteer_calls)
 
-  planeteer_calls.map { |element|  " #{element.capitalize} + "!"}
+  planeteer_calls.map { |element|  element.capitalize} + "!"}
 
-end 
+end
 
 
   # code an argument here
